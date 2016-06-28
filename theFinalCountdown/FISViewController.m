c6c9c74db995da8e8af61deb09ae9c0f17215824
hdinhof1 <hdinhof1@binghamton.edu>
2016-06-28 19:09:09 -0400

    Main part works. Still need to implement pause

--- theFinalCountdown/FISViewController.m
+++ theFinalCountdown/FISViewController.m
@@ -10,6 +10,12 @@
 
 @interface FISViewController ()
 
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UILabel *timeText;
+@property (weak, nonatomic) IBOutlet UIButton *pauseButton;
+@property (strong, nonatomic) NSTimer *countdownTimer;
+@property (nonatomic) NSInteger startingSeconds;
+
 @end
 
 @implementation FISViewController
@@ -18,6 +24,7 @@
 {
     [super viewDidLoad];
 
+    self.timeText.adjustsFontSizeToFitWidth = YES;
 
 
 
@@ -35,6 +42,50 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)startTapped:(id)sender {
+    self.datePicker.hidden = YES;
+    
+    [sender setTitle:@"Cancel" forState:UIControlStateNormal];
+    ((UIButton *)sender).tintColor = [UIColor redColor];
+    
+    self.pauseButton.enabled = YES;
+    self.timeText.hidden = NO;
+
+
+    self.startingSeconds = [self.datePicker countDownDuration];
+
+    
+    
+    //target:gonna call a method to invalidate sleep timer
+    self.countdownTimer = [NSTimer scheduledTimerWithTimeInterval:1.00 target:self selector:@selector(updateTimer:) userInfo:nil repeats:YES];
+ 
+}
+- (IBAction)pauseTapped:(id)sender {
+    
+}
+-(void)updateTimer:(NSTimer *)timer {
+    
+    if (self.startingSeconds <= 0) {
+        [timer invalidate];
+    }
+    else {
+        self.startingSeconds--;
+        NSString *timeFormatted = [self timeFormatted:self.startingSeconds];
+        self.timeText.text = timeFormatted;
+        NSLog(@"Timer is %@", timeFormatted);
+        
+    }
+    
+}
+- (NSString *)timeFormatted:(NSUInteger)totalSeconds
+{
+    
+    NSUInteger seconds = totalSeconds % 60;
+    NSUInteger minutes = (totalSeconds / 60) % 60;
+    NSUInteger hours = totalSeconds / 3600;
+    
+    return [NSString stringWithFormat:@"%02lu:%02lu:%02lu",(unsigned long)hours, (unsigned long)minutes, (unsigned long)seconds];
 }
 
 @end
